c4374185bac62ef97a9f0dfa1649cc39753724a3
Cola-Ace <73228853+Cola-Ace@users.noreply.github.com>
2021-12-07 18:33:47 +0800

    Add files via upload

--- ncmdump.py
+++ ncmdump.py
@@ -4,9 +4,24 @@ import base64
 import json
 import os
 import sys
+import datetime
 from Crypto.Cipher import AES
 
-def dump(file_path):
+def getNowtime():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+class logger:
+    def info(msg):
+        print(getNowtime() + " [Info] " + msg)
+    def error(msg):
+        print(getNowtime() + " [Error] " + msg)
+    def warning(msg):
+        print(getNowtime() + " [Warning] " + msg)
+    def debug(msg):
+        if "--debug" in sys.argv:
+            print(getNowtime() + " [Debug] " + msg)
+
+def dump(file_path, remove):
     # hex to str
     core_key = binascii.a2b_hex("687A4852416D736F356B496E62617857")
     meta_key = binascii.a2b_hex("2331346C6A6B5F215C5D2630553C2728")
@@ -71,17 +86,27 @@ def dump(file_path):
         m.write(chunk)
     m.close()
     f.close()
+    if remove:
+        os.remove(file_path)
     return file_name
 
 
 if __name__ == "__main__":
-    __list__ = sys.argv[1:]
-    if ("--no-output" in __list__):
-        del __list__[__list__.index("--no-output")]
+    args = sys.argv[1:]
+    __list__ = []
+    remove = False
+    if "--remove-source" in args:
+        remove = True
+    if "--all" in args:
+        files = os.listdir(os.getcwd())
+        for file in files:
+            if file.endswith(".ncm"):
+                __list__.append(file)
+    if "--no-output" in args:
         for file in __list__:
-            dump(file)
+            dump(file, remove)
     else:
         for file in __list__:
-            print("正在转换 (" + str(__list__.index(file) + 1) + "/" + str(len(__list__)) + "): " + file)
-            print("转换成功! 输出文件名为 " + dump(file))
-        print("转换完成! 共转换 " + str(len(__list__)) + " 个文件")
+            logger.info("正在转换 (" + str(__list__.index(file) + 1) + "/" + str(len(__list__)) + "): " + file)
+            logger.info("转换成功! 输出文件名为 " + dump(file, remove))
+        logger.info("转换完成! 共转换 " + str(len(__list__)) + " 个文件")
